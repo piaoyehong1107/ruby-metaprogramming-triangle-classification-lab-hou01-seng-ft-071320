5843a8f174e28f2e0931461040f8ce810846c82e
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-01 05:22:58 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  attr_accessor :first, :second, :third
+  def initialize (first,second,third)
+    @first=first
+    @second=second
+    @third=third
+  end
+  def kind 
+    if (@first == @second && @second == @third)
+      return :equilateral
+    elsif (@first == @second || @seond == @third)
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+    
+   class PartnerError < StandardError
+     if (@first + @second < @third || @second + @third <@first || @first + @third <@second)
+      begin
+		    raise TriangleError
+		  rescue TriangleError => error
+		 end
+		 end
+	 end
 end
